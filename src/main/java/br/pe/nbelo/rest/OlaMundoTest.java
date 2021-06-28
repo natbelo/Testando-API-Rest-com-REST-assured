8c2e65f9dbc056bf29388ada6855f1cfd7e3d4ee
de Oliveira <n.de.oliveira@accenture.com>
2021-06-28 13:43:19 -0300

    Rest Assured básico

---
+++ src/main/java/br/pe/nbelo/rest/OlaMundoTest.java
@@ -0,0 +1,44 @@
+package br.pe.nbelo.rest;
+
+import static io.restassured.RestAssured.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+public class OlaMundoTest {
+	
+	@Test
+	public void testOlaMundo() {
+		Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
+		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		Assert.assertTrue(response.statusCode() == 200);
+		Assert.assertTrue("O status code deveria ser 200", response.statusCode() == 200);
+		Assert.assertEquals(200, response.statusCode());
+		
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+	}
+	
+	@Test
+	public void devoConhecerOutrasFormasRestAssured() {
+		Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+		
+		get("https://restapi.wcaquino.me/ola").then().statusCode(200);
+		
+		given()
+		.when()
+			.get("https://restapi.wcaquino.me/ola")
+		.then()
+			//.assertThat()
+			.statusCode(200);
+	}
+	
+
+}
